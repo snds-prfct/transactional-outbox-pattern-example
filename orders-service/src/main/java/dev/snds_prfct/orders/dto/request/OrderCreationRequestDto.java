5dcabe469379d60ce2cabb388330c682b4267560
snds_prfct <snds.prfct@gmail.com>
2026-03-27 20:58:44 +0300

    API Documentation enhancement

--- orders-service/src/main/java/dev/snds_prfct/orders/dto/request/OrderCreationRequestDto.java
+++ orders-service/src/main/java/dev/snds_prfct/orders/dto/request/OrderCreationRequestDto.java
@@ -1,16 +1,21 @@
 package dev.snds_prfct.orders.dto.request;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.constraints.NotEmpty;
 import jakarta.validation.constraints.NotNull;
 
 import java.util.Map;
 import java.util.UUID;
 
+@Schema(title = "Request body for order creation request")
 public record OrderCreationRequestDto(
+        @Schema(example = "c25a7996-bd20-49e6-b79c-d358e4532091")
         @NotNull
         UUID idempotencyKey,
+        @Schema(example = "{\"1\": 2}")
         @NotEmpty
         Map<Long, Integer> productsAmountByProductId,
+        @Schema(example = "Country, City, Street, Building")
         @NotNull
         String deliveryAddress) {
 }
