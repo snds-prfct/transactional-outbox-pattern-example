cd992a1e423f98b4aa85817bd8e5b658c8a19440
Iura <snds.prfct@gmail.com>
2026-03-27 21:26:53 +0300

    API Documentation enhancement (#13)

---
+++ orders-service/src/main/java/dev/snds_prfct/orders/configuration/OpenApiConfiguration.java
@@ -0,0 +1,13 @@
+package dev.snds_prfct.orders.configuration;
+
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
+
+@OpenAPIDefinition(
+        info = @Info(
+                title = "Order Service API",
+                description = "Service for processing user orders"
+        )
+)
+public class OpenApiConfiguration {
+}
--- orders-service/src/main/java/dev/snds_prfct/orders/controller/OrderController.java
+++ orders-service/src/main/java/dev/snds_prfct/orders/controller/OrderController.java
@@ -5,9 +5,12 @@ import dev.snds_prfct.orders.dto.response.OrderCancelledResponseDto;
 import dev.snds_prfct.orders.dto.response.OrderCreatedResponseDto;
 import dev.snds_prfct.orders.dto.response.OrderResponseDto;
 import dev.snds_prfct.orders.service.OrderService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PatchMapping;
@@ -19,6 +22,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
+@Tag(name = "Order Operations")
 @RestController
 @RequestMapping("/orders")
 @RequiredArgsConstructor
@@ -26,7 +30,11 @@ public class OrderController {
 
     private final OrderService orderService;
 
-    @PostMapping
+    @Operation(summary = "Create new order", description = "Creates new order with provided info")
+    @PostMapping(
+            consumes = MediaType.APPLICATION_JSON_VALUE,
+            produces = MediaType.APPLICATION_JSON_VALUE
+    )
     public ResponseEntity<OrderCreatedResponseDto> createOrder(@Valid @RequestBody OrderCreationRequestDto orderCreationRequestDto) {
         OrderCreatedResponseDto orderCreatedResponseDto = orderService.createOrder(orderCreationRequestDto);
         return ResponseEntity
@@ -34,7 +42,10 @@ public class OrderController {
                 .body(orderCreatedResponseDto);
     }
 
-    @GetMapping
+    @Operation(summary = "Find all orders", description = "Returns all user's orders")
+    @GetMapping(
+            produces = MediaType.APPLICATION_JSON_VALUE
+    )
     public ResponseEntity<List<OrderResponseDto>> findOrders() {
         List<OrderResponseDto> orders = orderService.findOrders();
         return ResponseEntity
@@ -42,7 +53,11 @@ public class OrderController {
                 .body(orders);
     }
 
-    @GetMapping("/{orderId}")
+    @Operation(summary = "Find order by Id", description = "Returns specific order info by its Id")
+    @GetMapping(
+            path = "/{orderId}",
+            produces = MediaType.APPLICATION_JSON_VALUE
+    )
     public ResponseEntity<OrderResponseDto> findOrderById(@PathVariable Long orderId) {
         OrderResponseDto order = orderService.findOrder(orderId);
         return ResponseEntity
@@ -50,7 +65,11 @@ public class OrderController {
                 .body(order);
     }
 
-    @PatchMapping("/{orderId}/cancel")
+    @Operation(summary = "Cancel order by Id", description = "Cancels order with provided Id")
+    @PatchMapping(
+            path = "/{orderId}/cancel",
+            produces = MediaType.APPLICATION_JSON_VALUE
+    )
     public ResponseEntity<OrderCancelledResponseDto> cancelOrder(@PathVariable Long orderId) {
         OrderCancelledResponseDto orderCancelledResponseDto = orderService.cancelOrder(orderId);
         return ResponseEntity
